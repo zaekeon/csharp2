b29385f17bb4f6fd3e492ce2993fb89dc20df84b
zaekeon <zaekeon@gmail.com>
2016-01-25 11:56:22 -0600

    added dictionary exercise

---
+++ DictionaryExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> catalog = new Dictionary<int, string>();
+
+            catalog.Add(4, "The Best Bacon Book");
+            catalog.Add(6, "The worst bacon book");
+
+            foreach (KeyValuePair<int,string> item in catalog)
+            {
+                Console.WriteLine(item.Key);
+                Console.WriteLine(item.Value);
+            }
+
+        }
+    }
+}
