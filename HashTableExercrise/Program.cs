b7e69d2e2d54a6859efe785fc4df06fd6d73705a
zaekeon <zaekeon@gmail.com>
2016-01-25 12:02:24 -0600

    added hashtable exercise

--- HashTableExercrise/Program.cs
+++ HashTableExercrise/Program.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Collections;
 namespace HashTableExercrise
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Hashtable bacon = new Hashtable();
+
+            bacon.Add(208, 44.33f);
+            bacon.Add(322, 34.22f);
+            bacon.Add("YRC", "Packing List - Jan 28");
+
+            //remember for hastable it's a dictionaryentry
+            //for dictionary it's a keyvaluepair
+
+            foreach (DictionaryEntry item in bacon)
+            {
+                Console.WriteLine(item.Key + " " + item.Value);
+            }
+
         }
     }
 }
