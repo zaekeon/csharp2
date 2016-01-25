1af8ee00b18e1b815a326cddcbd7cc751b8b80a9
zaekeon <zaekeon@gmail.com>
2016-01-25 12:40:16 -0600

    added anonymous function

--- AnonymousFunctions/Program.cs
+++ AnonymousFunctions/Program.cs
@@ -8,8 +8,40 @@ namespace AnonymousFunctions
 {
     class Program
     {
+
+        delegate float Average(int x, int y);
         static void Main(string[] args)
         {
+
+            Helper.CheeseHelper cow = (string ovx, int y) => { Console.WriteLine(ovx + y); };
+
+            cow("onion", 5);
+
+            //delcare anonymous functin reference
+            Average average;
+
+            average = (int x, int y) =>
+            {
+                float z;
+                z = (x + y) / 2.0f;
+                return z;
+            };
+
+            //invoke anonymous function
+            Console.WriteLine(average(9, 2).ToString("N1"));
+
+            //reassign the body of the anonymous function.
+            average = (x, y) => (x + y) / 2.0f;
+
+            //invoke the anonymous function with the simplieified body
+            Console.WriteLine(average(9,2).ToString("N1"));
+
+            Console.ReadLine();
+
+
+
+
+
         }
     }
 }
