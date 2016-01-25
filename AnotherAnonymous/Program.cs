bd1a9e8c860e79fcdffb3af37119fb78d2baa053
zaekeon <zaekeon@gmail.com>
2016-01-25 12:49:10 -0600

    added another anonymous example using Func<>

---
+++ AnotherAnonymous/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherAnonymous
+{
+    class Program
+    {
+        public delegate void ShowOutput(string message);
+        static void Main(string[] args)
+        {
+            ShowOutput showOutput;
+
+            //the intput type can be inferred.
+            showOutput = input => Console.WriteLine(input);
+
+            showOutput("hello");
+
+            //an example using func<>.  Using Func<> we don't need to delcare a delegate.  We can use this generic instance.
+
+            Func<int, int, float> average2;
+
+            average2 = (x,y) => (x + 7) / 2.0f;
+
+            Console.WriteLine(average2(1,5).ToString("N"));
+            Console.ReadLine();
+
+
+        }
+    }
+}
