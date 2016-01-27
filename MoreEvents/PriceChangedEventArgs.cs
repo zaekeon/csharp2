26b691244e0a4ad62bb97563cd4180e1b0d07930
zaekeon <zaekeon@gmail.com>
2016-01-27 14:26:16 -0600

    added another event example

---
+++ MoreEvents/PriceChangedEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreEvents
+{
+    class PriceChangedEventArgs : System.EventArgs
+    {
+        public readonly decimal LastPrice;
+        public readonly decimal NewPrice;
+
+        public PriceChangedEventArgs (decimal lastPrice, decimal newPrice)
+        { LastPrice = lastPrice;
+
+            NewPrice = newPrice;
+        }
+    }
+}
---
+++ MoreEvents/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreEvents
+{
+    class Program
+    {
+        //this is not a good example.  these events/methods should be in a different class.
+        public delegate void PriceChanged(object sender, PriceChangedEventArgs args);
+        public event PriceChanged PriceChangedEvent;
+        static void Main(string[] args)
+        {
+            Program bacon = new Program();
+
+            bacon.PriceChangedEvent += bacon.PriceChangedEventSubscriber;
+
+            //raise the event
+            bacon.OnPriceChanged();
+        }
+
+        protected virtual void OnPriceChanged()
+        {
+
+            PriceChangedEventArgs args = new PriceChangedEventArgs(2, 4);
+            if (PriceChangedEvent !=null)
+            {
+                PriceChangedEvent(this, args);
+            }
+        }
+
+        public void PriceChangedEventSubscriber(object sender, PriceChangedEventArgs args)
+        {
+            Console.WriteLine("Old price was :" + args.LastPrice + " New price is: " + args.NewPrice);
+        }
+    }
+}
