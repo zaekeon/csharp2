b9b83f8b53f432230ca72634b85f913feed7aebb
zaekeon <zaekeon@gmail.com>
2016-01-29 09:14:14 -0600

    added factorymethodpatternexamples

---
+++ FactoryMethodPattern/ConcreteProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodPattern
+{
+    class ConcreteProduct : Product
+    {
+        public override void TestClass()
+        {
+            Console.WriteLine("data from the concrete class");
+        }
+    }
+}
