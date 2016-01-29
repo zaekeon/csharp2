5bc8267527eeab3f836e09e355c4123f4db53164
zaekeon <zaekeon@gmail.com>
2016-01-29 07:44:30 -0600

    Added async/await example

---
+++ AysncAndAwait/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AysncAndAwait
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //label1.Text = BigLongImportantMethod("John");
+
+            // Task.Factory.StartNew(() => BigLongImportantMethod("Sally")).ContinueWith(t=> label1.Text = t.Result, TaskScheduler.FromCurrentSynchronizationContext());
+
+            CallBigImportantMethod();
+            label1.Text = "Waiting...";
+        }
+
+        private async void CallBigImportantMethod()
+        {
+            var result = await BigLongImportantMethodAsync("Andrew");
+            label1.Text = result;
+        }
+
+        private Task<string> BigLongImportantMethodAsync(string name)
+        {
+            return Task.Factory.StartNew(() => BigLongImportantMethod(name));
+        }
+        private string BigLongImportantMethod(string name)
+        {
+            Thread.Sleep(2000);
+            return "Hello, " + name;
+        }
+    }
+}
