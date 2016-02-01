473b856a27650c4f25aeb070cd751b81bce1a3b2
zaekeon <zaekeon@gmail.com>
2016-02-01 13:48:52 -0600

    added enum example

---
+++ EnumExample/EnumCows.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumExample
+{
+  public enum Cow { Milk, Fat, Beef, Onion}
+}
---
+++ EnumExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Cow.Milk);
+            Func<int,int> cheese = x => x + 1;
+            int cowFiber = cheese(4);
+        }
+    }
+}
